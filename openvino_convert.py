cc9906c1d0cc7897add42f02dabffb8b023661db
Edward Arthur Quarm Jnr <equarmjn@redhat.com>
2025-03-31 14:03:04 -0400

    update readme (#2)
    Co-authored-by: Edward Arthur Quarm Jnr <equarmjn@equarmjn-thinkpadp1gen5.rht.csb>

--- openvino_convert.py
+++ openvino_convert.py
@@ -1,5 +1,6 @@
 import torchvision
 import openvino as ov
+import torch
 
 if __name__ == "__main__":
 
@@ -7,7 +8,7 @@ if __name__ == "__main__":
     model = torchvision.models.densenet161(weights='DEFAULT')
 
     # Convert to OpenVINO format
-    ov_model = ov.convert_model(model)
+    ov_model = ov.convert_model(model, example_input=torch.randn(1, 3, 224, 224))
 
     # Save the OpenVINO model
     ov.serialize(ov_model, "openvino_model/model.xml", "openvino_model/model.bin")
